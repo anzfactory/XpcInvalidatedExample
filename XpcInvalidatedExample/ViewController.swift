fd86b5f1bdb9e37dae3e597e14a92638a636fc0c
anzfactory <anz.factory@gmail.com>
2018-11-28 20:11:58 +0900

    first commit

---
+++ XpcInvalidatedExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XpcInvalidatedExample
+//
+//  Created by sasato on 2018/11/28.
+//  Copyright © 2018年 sasato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
